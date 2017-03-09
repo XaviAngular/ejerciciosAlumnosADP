5bf4864679bad655300ec0887802a2b39b8ccfd8
ADP02-PC09\ADP <ainatpujolrigual@gmail.com>
2017-03-09 17:10:38 +0100

    ejercicio acabado

---
+++ TaniaPujol/Ejercicio Form/js/envio.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+   			$('select').material_select();
+   			$('.datepicker').pickadate({
+    			selectMonths: true, // Creates a dropdown to control month
+    			selectYears: 2 // Creates a dropdown of 15 years to control year
+  			});
+  			$('input.autocomplete').autocomplete({
+    			data: {
+      				"Barcelona": null,
+      				"Reus": null,
+      				"Girona": null,
+      				"Madrid": null,
+      				"Santiago de Compostela": null,
+      				"Hawai": null,
+      				"Shangai": null,
+      				"New York": null,
+      				"Paris": null,
+      				"Roma": null,
+      				"Tokyo": null,
+       			},
+    			limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+  			});
+  		$.ajax({
+        url: 'http://www.travek.com/search',
+        type: 'GET',
+        dataType: 'json',
+        data: jsonData,
+       .success(function() {
+          console.log("campos");
+         console.log("valores");
+        })
+      .error(function() {
+        console.log("error");
+      })
+    });
+      
+      
+		        
+		  });
